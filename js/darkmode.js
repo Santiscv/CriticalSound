0abb3dbc9415b400cc98c339e1416721564c0272
santiscv <carranzasantiago85@gmail.com>
2024-09-08 20:36:40 -0300

    The error was resolved to ask if the two buttons exist in the same domain on different pages.

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,26 +1,34 @@
-const body = document.body;
-const btnDarkMode = document.getElementById("dark_mode");
-const btnDarkModeResponsive = document.getElementById("dark_mode_responsive");
+document.addEventListener("DOMContentLoaded", (event) => {
+    const body = document.body;
+    const btnDarkMode = document.getElementById("dark_mode");
+    const btnDarkModeResponsive = document.getElementById("dark_mode_responsive");
 
-// Función para manejar el cambio de modo oscuro
-toggleDarkMode=() => {
-    body.classList.toggle("dark");
+    if (btnDarkMode || btnDarkModeResponsive) {
+        // Función para manejar el cambio de modo oscuro
+        const toggleDarkMode = () => {
+            body.classList.toggle("dark");
 
-    // Guardar el estado en localStorage
-    if (body.classList.contains("dark")) {
-        localStorage.setItem("dark-mode", "true");
-    } else {
-        localStorage.setItem("dark-mode", "false");
+            // Guardar el estado en localStorage
+            if (body.classList.contains("dark")) {
+                localStorage.setItem("dark-mode", "true");
+            } else {
+                localStorage.setItem("dark-mode", "false");
+            }   
+        };
+
+        // Asignar la función a ambos botones    
+        if (btnDarkMode) {
+            btnDarkMode.addEventListener("click", toggleDarkMode);
+        }
+        if (btnDarkModeResponsive) {
+            btnDarkModeResponsive.addEventListener("click", toggleDarkMode);
+        }
+
+        // Obtener el estado guardado en localStorage y aplicarlo
+        if (localStorage.getItem("dark-mode") === "true") {
+            body.classList.add("dark");
+        } else {
+            body.classList.remove("dark");
+        }
     }
-};
-
-// Asignar la función a ambos botones
-btnDarkMode.addEventListener("click", toggleDarkMode);
-btnDarkModeResponsive.addEventListener("click", toggleDarkMode);
-
-// Obtener el modo actual
-if (localStorage.getItem("dark-mode") === "true") {
-    body.classList.add("dark");
-} else {
-    body.classList.remove("dark");
-}
+});
