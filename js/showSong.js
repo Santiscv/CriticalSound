2797e6aaf5b96a7afb8e5c6b1fc7c67f68f8c3c3
mcarr128 <167709819+mcarr128@users.noreply.github.com>
2024-09-08 14:43:04 -0300

    billboard template version 1

--- js/showSong.js
+++ js/showSong.js
@@ -13,3 +13,20 @@ for(song of songsRatings){
     let classSong = new Song(song);
     musicRatingsContainer.innerHTML += classSong.getTemplate();
 }
+
+
+const mostLikedList = getmostLikedSongs()
+const mostCriticizedList = getmostCriticizedSongs()
+const likedContainer= document.getElementById("liked-container");
+const criticizedContainer = document.getElementById("criticized-container")
+watchlistContainer.innerHTML="";
+musicRatingsContainer.innerHTML="";
+
+for(song of mostLikedList){ /*falta un let dentro del for?*/
+    let classSong = new Song(song);
+    likedContainer.innerHTML += classSong.getTemplate();
+}
+for(song of mostCriticizedList){
+    let classSong = new Song(song);
+    criticizedContainer.innerHTML += classSong.getTemplate();
+}
--- js/songsRepository.js
+++ js/songsRepository.js
@@ -84,3 +84,91 @@ function getmusicRatingsSongs(){
         id: 10
     }];
 } 
+
+function getmostLikedSongs(){
+    return[{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 1
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 2
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 3
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 4
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 5
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 6
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 7
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 8
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 9
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 10
+    }];
+} 
+
+function getmostCriticizedSongs(){
+    return[{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 1
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 2
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 3
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 4
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 5
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 6
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 7
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 8
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 9
+    },{
+        imageURL: "images/songs-min.jpg",
+        description:"reference image of the music ratings",
+        id: 10
+    }];
+} 
