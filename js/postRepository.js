a318ef84c96dc9ea5ee1cc0104746b1791857bea
Marcos Monjes <marcos.monjes@itscordoba.edu.ar>
2024-09-09 14:06:47 -0300

    First preview of diname view on homepage

---
+++ js/postRepository.js
@@ -0,0 +1,30 @@
+function getUserProfile() {
+    return [{
+        userImageUrl: "images/profile(icono)-min.png",
+        username: "Username",
+        id: 1
+    }];
+}
+
+function getPosts() {
+    return [{
+        postImageUrl: "images/user-post-min.jpg",
+        postDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        id: 1
+    }];
+}
+function getComment() {
+    return [{
+        imgUserComment: "images/profile(icono)-min.png",
+        usernameComment: "Username :  ",
+        userId: 1 ,
+        userComment: "This is a comment xd..",
+        commentId: 1
+    }];
+}
+function getUserMakeComment() {
+    return [{
+        imgUserForCommenting: "images/profile(icono)-min.png",
+        id: 1
+    }];
+}
---
+++ js/posts.js
@@ -0,0 +1,80 @@
+class UserProfile {
+    constructor(userProfile) {
+        this.userImageUrl = userProfile.userImageUrl;
+        this.username = userProfile.username;
+        this.userId = userProfile.userId
+    }
+
+    getTemplate = () => {
+        return `
+            <article class="user-profile">
+                <img 
+                src="${this.userImageUrl}" 
+                alt="user-profile-image" 
+                title="User-Profile"
+                >
+                <h3>${this.username}</h3>
+            </article>
+            `;
+    }  
+}
+
+class Post {
+    constructor(posts) {
+        this.postImageUrl = posts.postImageUrl;
+        this.postDescription = posts.postDescription;
+        this.id = posts.id;
+    }
+
+    getTemplate = () => {
+        return `
+            <article class="post-imagen">
+                <img
+                src="${this.postImageUrl}" 
+                alt="friend image"
+                >
+                <p>
+                    ${this.postDescription}
+                </p>
+            </article>
+            `;
+    }
+}
+
+class Comment { 
+    constructor(comments) {
+        this.imgUserComment = comments.imgUserComment;
+        this.usernameComment = comments.usernameComment;
+        this.userId = comments.userId;
+        this.userComment = comments.userComment;
+        this.commentId = comments.commentId;
+    }
+
+    getTemplate = () => {
+        return `
+            <div class="comment">
+                <img 
+                src="${this.imgUserComment}" 
+                alt="profile-image">
+                <h4>${this.usernameComment}</h4>
+                <p>${this.userComment}</p>
+            </div>
+            `;
+    }
+}
+
+class MakeComment {
+    constructor(makeComments) {
+        this.imgUserForCommenting = makeComments.imgUserForCommenting;
+        this.id = makeComments.id
+    }
+
+    getTemplate = () => {
+        return `
+            <img 
+            src="${this.imgUserForCommenting}" 
+            alt="profile-image"
+            >
+            `;
+    }
+}
