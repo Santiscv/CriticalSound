37fcfab76ddde9dea2f996eedd043576e6fd499c
mcarr128 <167709819+mcarr128@users.noreply.github.com>
2024-09-15 12:50:14 -0300

    Santi's if added in show song file

--- js/showSong.js
+++ js/showSong.js
@@ -1,32 +1,40 @@
-const songsWatchlist = getwatchlistSongs()
-const songsRatings = getmusicRatingsSongs()
-const watchlistContainer= document.getElementById("watchlist-container");
-const musicRatingsContainer = document.getElementById("music-ratings-container")
-watchlistContainer.innerHTML="";
-musicRatingsContainer.innerHTML="";
+document.addEventListener("DOMContentLoaded", (event) => {
+    const songsWatchlist = getwatchlistSongs();
+    const songsRatings = getmusicRatingsSongs();
+    const watchlistContainer = document.getElementById("watchlist-container");
+    const musicRatingsContainer = document.getElementById("music-ratings-container");
+    
+    if (watchlistContainer && musicRatingsContainer) {
+        watchlistContainer.innerHTML = "";
+        musicRatingsContainer.innerHTML = "";
 
-for(song of songsWatchlist){
-    let classSong = new Song(song);
-    watchlistContainer.innerHTML += classSong.getTemplate();
-}
-for(song of songsRatings){
-    let classSong = new Song(song);
-    musicRatingsContainer.innerHTML += classSong.getTemplate();
-}
+        for (song of songsWatchlist) { 
+            let classSong = new Song(song);
+            watchlistContainer.innerHTML += classSong.getTemplate();
+        }
 
-
-const mostLikedList = getmostLikedSongs()
-const mostCriticizedList = getmostCriticizedSongs()
-const likedContainer= document.getElementById("liked-container");
-const criticizedContainer = document.getElementById("criticized-container")
-watchlistContainer.innerHTML="";
-musicRatingsContainer.innerHTML="";
-
-for(song of mostLikedList){ /*falta un let dentro del for?*/
-    let classSong = new Song(song);
-    likedContainer.innerHTML += classSong.getTemplate();
-}
-for(song of mostCriticizedList){
-    let classSong = new Song(song);
-    criticizedContainer.innerHTML += classSong.getTemplate();
-}
+        for (song of songsRatings) {
+            let classSong = new Song(song);
+            musicRatingsContainer.innerHTML += classSong.getTemplate();
+        }
+    }else{
+        const mostLikedList = getmostLikedSongs();
+        const mostCriticizedList = getmostCriticizedSongs();
+        const likedContainer = document.getElementById("liked-container");
+        const criticizedContainer = document.getElementById("criticized-container");
+        if (likedContainer && criticizedContainer) {
+            likedContainer.innerHTML = "";
+            criticizedContainer.innerHTML = "";
+    
+            for (song of mostLikedList) {
+                let classSong = new Song(song);
+                likedContainer.innerHTML += classSong.getTemplate();
+            }
+    
+            for (song of mostCriticizedList) {
+                let classSong = new Song(song);
+                criticizedContainer.innerHTML += classSong.getTemplate();
+            }
+        }
+    }
+});
