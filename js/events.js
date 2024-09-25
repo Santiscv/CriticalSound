160a6da2da7b66c61a683f493fb3cffc5bca5d7b
Santiago Carranza <carranzasantiago85@gmail.com>
2024-09-25 13:28:40 -0300

    The likecounter file was deleted and the code was sent to events

--- js/events.js
+++ js/events.js
@@ -12,4 +12,14 @@ document.addEventListener("submit", (e) => {
     alert(JSON.stringify(data));
 
 })
+// ---------------Contador de Likes--------------------------
+const showLikes = document.getElementById("counter");
+let likeCounter = 0;
+let iconLike = document.querySelector('.like-logo')
+
+iconLike.addEventListener('click', () => {
+    likeCounter++;
+    showLikes.textContent = likeCounter;
+});
+
 
--- js/likeCounter.js
+++
@@ -1,9 +0,0 @@
-let showLikes = document.getElementById("counter");
-let likeCounter = 0;
-let iconLike = document.querySelector('.like-logo')
-
-iconLike.addEventListener('click', () => {
-    likeCounter++;
-    showLikes.textContent = likeCounter;
-});
-
