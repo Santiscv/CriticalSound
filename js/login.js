248fe3110fedef3b0179d012bf09f613b4677f7d
Marcos Monjes <marcos.monjes@itscordoba.edu.ar>
2024-09-28 11:42:04 -0300

    What was seen in programming class 3 on 09-26-24 was implemented: login.js and loginRepository.js files and their link in index.html for the validation of access to the user site and connection to the Database.

---
+++ js/login.js
@@ -0,0 +1,25 @@
+let formLogin = document.getElementById("frm_user_data");
+
+formLogin.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let txtName = document.getElementById("txt_name");
+    let txtPassword = document.getElementById("txt_password");
+
+    let pedido = {
+        mail: txtName.value,
+        password: txtPassword.value
+    };
+
+    getLogin(pedido, 
+        (json) => {
+            if(json.success) {
+                window.location.href = "/homepage.html";
+            }else {
+                let errorLabel = document.getElementById("error_label");
+                errorLabel.style.display = "block";
+                errorLabel.innerText = `* ${json.message}`;
+            }
+            
+        });
+});
---
+++ js/loginRepository.js
@@ -0,0 +1,12 @@
+function getLogin(pedido, success) {
+    fetch('http://localhost:5088/login', {
+        method: 'POST',
+        body: JSON.stringify(pedido),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then((response) => response.json())
+        .then((json) => success(json))
+        .catch((error) => console.log(error));
+}
