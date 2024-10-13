9a5fd1a7fc86d756f4c1cd4c082510ec6a3ae289
Santiago Carranza <carranzasantiago85@gmail.com>
2024-10-12 21:38:59 -0300

    a fetch is performed from the database to create a new user from the register controller

--- js/register.js
+++ js/register.js
@@ -3,8 +3,28 @@ let signUp = document.getElementById("form-registration");
 signUp.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    signUp.reset();
-    document.querySelector('input[type="checkbox"]').checked = false;
-    
-    alert("Your account has been successfully registered!")
+    let txtUserName= document.getElementById("user");
+    let txtUserBirthdate= document.getElementById("date");
+    let txtUserMail= document.getElementById("email");
+    let txtUserPassword= document.getElementById("password");
+
+    let pedido={
+        userName: txtUserName.value,
+        birthdate: txtUserBirthdate.value,
+        mail: txtUserMail.value,
+        password: txtUserPassword.value
+    }
+    getLogin(pedido, 
+        (json) => {
+            if(json.success) {
+                window.location.href = "/homepage.html";
+            }
+            // }else {
+            //     let errorLabel = document.getElementById("error_label");
+            //     errorLabel.style.display = "block";
+            //     errorLabel.innerText = `* ${json.message}`;
+            // }
+            
+        });
 });
+
